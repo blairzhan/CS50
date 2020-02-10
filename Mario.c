d44910d1c686d0ff43c042576e783fcd87856261
blairzhan <60574590+blairzhan@users.noreply.github.com>
2020-02-09 20:47:23 -0800

    Create Mario.c

---
+++ Mario.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int("positive number: ");
+     for (int i = 0; i< n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+
